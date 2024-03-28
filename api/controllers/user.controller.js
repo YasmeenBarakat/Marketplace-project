b2e12fe5e1c75eece96c4af7763503d53c366fb2
yasmeen.barakat <yasmine01barakt@gmail.com>
2024-03-28 19:32:09 +0200

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    messsage: "we will get the user",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 const app = express();
 mongoose
@@ -11,6 +12,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRouter);
 app.listen(3000, () => {
   console.log("Server running in port 3000");
 });
